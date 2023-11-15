3db83ef0ad85eef68014965f1466912d0243380c
RuyHa <code.ruyha@gmail.com>
2023-11-15 17:59:28 +0900

    [feat] make project

---
+++ SwiftUI_Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Ruyha on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
